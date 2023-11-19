3e8214613b198631e727806bf99f895cc5109c89
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-11-19 19:08:01 +0100

    update

--- twitter-kafka-service/src/main/java/com/sdia/twitterkafkaservice/TwitterKafkaServiceApplication.java
+++ twitter-kafka-service/src/main/java/com/sdia/twitterkafkaservice/TwitterKafkaServiceApplication.java
@@ -1,13 +1,23 @@
 package com.sdia.twitterkafkaservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwitterKafkaServiceApplication {
+public class TwitterKafkaServiceApplication implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TwitterKafkaServiceApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(TwitterKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        LOG.info("App starts...");
     }
 
 }
