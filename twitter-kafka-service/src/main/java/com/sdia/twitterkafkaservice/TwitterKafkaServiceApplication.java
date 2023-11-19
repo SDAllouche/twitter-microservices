66b8817f93bf03c2163e5956b959aa09e74ff516
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-11-19 19:04:17 +0100

    initialise

---
+++ twitter-kafka-service/src/main/java/com/sdia/twitterkafkaservice/TwitterKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sdia.twitterkafkaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterKafkaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterKafkaServiceApplication.class, args);
+    }
+
+}
