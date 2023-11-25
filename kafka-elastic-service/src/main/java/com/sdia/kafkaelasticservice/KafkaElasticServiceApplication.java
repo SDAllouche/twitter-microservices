3d55afe19e919ccfd86dbde60d761854d23c4d28
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-11-25 22:30:44 +0100

    update

---
+++ kafka-elastic-service/src/main/java/com/sdia/kafkaelasticservice/KafkaElasticServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sdia.kafkaelasticservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaElasticServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaElasticServiceApplication.class, args);
+    }
+
+}
---
+++ kafka-elastic-service/src/test/java/com/sdia/kafkaelasticservice/KafkaElasticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdia.kafkaelasticservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaElasticServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
